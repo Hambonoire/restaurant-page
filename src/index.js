be476989e0ef9a0bea10961d877ca34c3f1d9c95
Landryman <rtlandry87@gmail.com>
2025-04-30 16:21:13 -0500

    no major changes. removed some comments and unused lines of code.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import "./styles.css";
-import { greeting, pageContent } from "./script.js";
-// import { script.js } from ""
+import { pageContent } from "./script.js";
 
-// import hamburgerImage from "images/eiliv-aceron-pu6b4yIlQF4-unsplash.jpg";
-   
-// const image = document.createElement("img");
-// image.src = hamburgerImage;
-   
-// document.body.appendChild(image);
+pageContent();
 
+// Or
 
-alert(greeting);
-alert(pageContent());
