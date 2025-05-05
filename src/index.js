ad311140d6f6ebf689e34dd4e9b9365488cb02cb
Landryman <rtlandry87@gmail.com>
2025-05-04 19:19:44 -0500

    update code to reflect the refactor in home.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import { homeContent } from "./home.js";
 import { menuContent } from "./menu.js";
 import { aboutContent } from "./about.js";
 
+homeContent().displayPage();
 
-function pageToLoad(def = homeContent, newPage) {
+const navChildren = homeContent().getNavBtns();
 
+const tabToLoad = (newPage) => {
+    console.log("pageToLoad called");
     console.log("newPage: ", newPage);
     if (newPage === "Home") {
         homeContent();
@@ -12,32 +15,33 @@ function pageToLoad(def = homeContent, newPage) {
         menuContent();
     } else if (newPage === "About") {
         aboutContent();
-    } else {
-        def();
     }
 };
 
-const tabsContainer = pageContent().children;
-console.log(tabsContainer);
 const tabClickHandler = (e) => {
     
     console.log(e.target.textContent);
 
     if (e.target.textContent === "Home") {
         console.log("Home tab clicked");
-        pageToLoad(e.target.textContent);
+        tabToLoad(e.target.textContent);
     } else if (e.target.textContent === "Menu") {
         console.log("Menu tab clicked");
-        pageToLoad(e.target.textContent);
+        tabToLoad(e.target.textContent);
     } else if (e.target.textContent === "About") {
         console.log("About tab clicked");
-        pageToLoad(e.target.textContent);
+        tabToLoad(e.target.textContent);
     }
-}
+};
 
-Array.from(tabsContainer).forEach(tab => {
-    tab.addEventListener('click', tabClickHandler);
-});
+const loopNavs = () => {
+    for (let nav in navChildren) {
 
-pageToLoad();
+        navChildren[nav].addEventListener("click", tabClickHandler);
+    };
+};
+
+loopNavs();
+
+
 
