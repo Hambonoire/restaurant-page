7f21ab68a69ee2c7a2c26d77cac351e67e7e78ca
Landryman <rtlandry87@gmail.com>
2025-05-10 15:59:58 -0500

    change imports to match names of exports -- remove redundant code -- renamed variable 'tabToLoad' to 'loadNewPage' -- renamed 'tabToLoad' parameter to 'newPage'

--- src/index.js
+++ src/index.js
@@ -1,47 +1,52 @@
-import { homeContent } from "./home.js";
-import { menuContent } from "./menu.js";
-import { aboutContent } from "./about.js";
+import { getNavBtns } from "./header.js";
+import { home } from "./home.js";
+import { menu } from "./menu.js";
+import { about } from "./about.js";
 
-homeContent().displayPage();
+export let getPageToLoad;
 
-const navChildren = homeContent().getNavBtns();
+const loadPage = (a = home(), b) => {
+    
+    if (a >= 0 && a <= 2) {
+        b();
+    } else {
+        a();
+    };
+};
 
-const tabToLoad = (newPage) => {
+const loadNewPage = (tabClick) => {
     console.log("pageToLoad called");
-    console.log("newPage: ", newPage);
-    if (newPage === "Home") {
-        homeContent();
-    } else if (newPage === "Menu") {
-        menuContent();
-    } else if (newPage === "About") {
-        aboutContent();
+    console.log("newPage: ", tabClick);
+    if (tabClick === "Home") {
+        home();
+        loadPage(getPageToLoad);
+    } else if (tabClick === "Menu") {
+        menu();
+        loadPage(getPageToLoad);
+    } else if (tabClick === "About") {
+        about();
+        loadPage(getPageToLoad);
     }
 };
 
 const tabClickHandler = (e) => {
     
     console.log(e.target.textContent);
-
+    
     if (e.target.textContent === "Home") {
         console.log("Home tab clicked");
-        tabToLoad(e.target.textContent);
+        loadNewPage(e.target.textContent);
     } else if (e.target.textContent === "Menu") {
         console.log("Menu tab clicked");
-        tabToLoad(e.target.textContent);
+        loadNewPage(e.target.textContent);
     } else if (e.target.textContent === "About") {
         console.log("About tab clicked");
-        tabToLoad(e.target.textContent);
+        loadNewPage(e.target.textContent);
     }
 };
 
-const loopNavs = () => {
-    for (let nav in navChildren) {
-
-        navChildren[nav].addEventListener("click", tabClickHandler);
-    };
-};
-
-loopNavs();
-
+getNavBtns().navBtn1.addEventListener("click", tabClickHandler);
+getNavBtns().navBtn2.addEventListener("click", tabClickHandler);
+getNavBtns().navBtn3.addEventListener("click", tabClickHandler);
 
 
