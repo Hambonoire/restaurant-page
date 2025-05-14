6fa8cccee81ea4616e4e80e52cbcb2bf515cbac5
Landryman <rtlandry87@gmail.com>
2025-05-13 19:20:36 -0500

    add all default page load styles to pageDefaults.js -- called in index.js

--- src/pageDefaults.js
+++ src/pageDefaults.js
@@ -1,15 +1,33 @@
+import hambergerImage from "./images/hamberger.jpg";
 export const pageDefaults = () => {
-
-    const style = document.createElement('style');
-
-    style.innerHTML = `
-    * { margin: 0; padding: 0; }
+  
+    document.head.setAttribute("style", `
+    * { margin: 0; padding: 0; };
     ::-webkit-scrollbar {
         display: none;
     }
-    `;
+    `);
 
-    document.head.appendChild(style);
+    document.body.setAttribute("style", `display: flex; 
+        display-direction: column; height: 100vh;
+        overflow-x: hidden;`);
+
+    const body = document.querySelector("body");
+    const header = document.getElementById('#header');
+
+    const backgroundImage = document.createElement("div");
+
+    console.log(backgroundImage);
+    backgroundImage.setAttribute("id", "bg-image");
+    backgroundImage.setAttribute("style", `display: inherit; 
+        flex-direction: inherit; height: 100%; 
+        background-image: url(${hambergerImage}); 
+        backdrop-filter: brightness(50%); background-size: cover; 
+        background-position: center; background-repeat: no-repeat;`
+    );
+
+    body.insertBefore(backgroundImage, header);
+    
 };
 
 
