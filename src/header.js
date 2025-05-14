eea744ccaa06a8a49955aeebb2c6c48502ff72b3
Landryman <rtlandry87@gmail.com>
2025-05-14 17:42:38 -0500

    change text color from black to white

--- src/header.js
+++ src/header.js
@@ -1,21 +1,22 @@
 const headerEls = () => {
 
     const headerContainer = document.querySelector("#header");
-    headerContainer.setAttribute("style", `display: flex; justify-content: space-between; 
-        background-color: transparent; padding: 20px 40px`);
+    headerContainer.setAttribute("style", `display: flex; 
+        justify-content: space-between; 
+        align-items: center; padding: 20px 40px; height: 10%`);
 
     const nav = document.querySelector("nav");
     nav.setAttribute("id", "nav");
-    nav.setAttribute("style", `display: flex; align-items: center; padding: 0 20px;
-        gap: 10px`);
+    nav.setAttribute("style", `display: flex; align-items: center; 
+        padding: 0 20px; gap: 10px`);
 
     const navBtns = nav.children;
 
     for (let i = 0; i < navBtns.length; i++) {
-        navBtns[i].setAttribute("style", `background-color: transparent; border: none; 
-            padding: 0; font-family: inherit; 
+        navBtns[i].setAttribute("style", `background-color: transparent; 
+            border: none; padding: 0; font-family: inherit; 
             font-weight: bold; font-style: italic; 
-            font-size: 18px; color: white; padding: 10px;`)
+            font-size: 1.5rem; color: white; padding: 10px;`);
         navBtns[i].className = `navBtn${i+1}`;
         if (i === 0) {
             navBtns[i].textContent = "Home";
@@ -34,8 +35,8 @@ const headerLogo = () => {
     logo.setAttribute("id", "logo");
     logo.textContent = "Le Hambonoire";
     logo.setAttribute("style", `display: flex; justify-content: center; 
-        align-items: center; font-style: italic; color: white; font-size: 3em; 
-        padding: 10px 20px; margin: 0;`);
+        align-items: center; font-style: italic; color: white; 
+        font-size: 2.5rem; padding: 10px 20px; margin: 0; text-wrap: nowrap;`);
 
     return {logo};
 };
