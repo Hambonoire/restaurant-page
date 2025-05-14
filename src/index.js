7e72e46ccf0fc137fb743ae8e05804a1d4cd109d
Landryman <rtlandry87@gmail.com>
2025-05-13 19:27:45 -0500

    refactor to match changes made in pageDefaults.js and header.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,36 @@
-import { getNavBtns } from "./header.js";
-import { home } from "./home.js";
-import { menu } from "./menu.js";
-import { about } from "./about.js";
+import { pageDefaults } from "./pageDefaults.js";
+import { header } from "./header.js";
+// import { home } from "./home.js";
+// import { menu } from "./menu.js";
+// import { about } from "./about.js";
 
-export let getPageToLoad;
-
-const loadPage = (a = home(), b) => {
-    
-    if (a >= 0 && a <= 2) {
-        b();
-    } else {
-        a();
-    };
-};
-
-const loadNewPage = (tabClick) => {
-    console.log("pageToLoad called");
-    console.log("newPage: ", tabClick);
-    if (tabClick === "Home") {
-        home();
-        loadPage(getPageToLoad);
-    } else if (tabClick === "Menu") {
-        menu();
-        loadPage(getPageToLoad);
-    } else if (tabClick === "About") {
-        about();
-        loadPage(getPageToLoad);
-    }
-};
+pageDefaults();
+header();
 
 const tabClickHandler = (e) => {
     
+    console.log(e.target);
     console.log(e.target.textContent);
     
     if (e.target.textContent === "Home") {
         console.log("Home tab clicked");
-        loadNewPage(e.target.textContent);
+        home();
     } else if (e.target.textContent === "Menu") {
         console.log("Menu tab clicked");
-        loadNewPage(e.target.textContent);
+        menu();
     } else if (e.target.textContent === "About") {
         console.log("About tab clicked");
-        loadNewPage(e.target.textContent);
+        about();
     }
 };
 
-getNavBtns().navBtn1.addEventListener("click", tabClickHandler);
-getNavBtns().navBtn2.addEventListener("click", tabClickHandler);
-getNavBtns().navBtn3.addEventListener("click", tabClickHandler);
+// getNavs.navBtns === { [navBtn1, navBtn2, navBtn3] };
+const navBtn1 = document.querySelector(".navBtn1");
+const navBtn2 = document.querySelector(".navBtn2");
+const navBtn3 = document.querySelector(".navBtn3");
+
+navBtn1.addEventListener("click", tabClickHandler);
+navBtn2.addEventListener("click", tabClickHandler);
+navBtn3.addEventListener("click", tabClickHandler);
 
 
