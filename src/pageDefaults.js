614f73ef0142c8a9b5068b5cc84a72ef005aaf6f
Landryman <rtlandry87@gmail.com>
2025-05-14 17:39:06 -0500

    move logic of background image into body element -- remove code previously related to #bg-image div

--- src/pageDefaults.js
+++ src/pageDefaults.js
@@ -9,25 +9,15 @@ export const pageDefaults = () => {
     `);
 
     const body = document.querySelector("body");
-    body.setAttribute("style", `height: 100vh; overflow-x: hidden;`);
-
-    const header = document.getElementById('#header');
+    body.setAttribute("style", `height: 100vh; overflow-x: hidden; 
+        background-image: url(${hambergerImage}); 
+        backdrop-filter: brightness(50%); background-size: cover; 
+        background-position: center; background-repeat: no-repeat;`);
 
     const contentContainer = document.querySelector("#content");
     contentContainer.setAttribute("style", `display: flex; flex-direction: 
         column; justify-content: space-between; align-items:center; gap: 20px; 
         padding-top: 150px;`);
-
-    const backgroundImage = document.createElement("div");
-    backgroundImage.setAttribute("id", "bg-image");
-    backgroundImage.setAttribute("style", `display: inherit; 
-        flex-direction: inherit; height: 100%; justify-content: inherit;
-        align-items: inherit; background-image: url(${hambergerImage}); 
-        backdrop-filter: brightness(50%); background-size: cover; 
-        background-position: center; background-repeat: no-repeat;`
-    );
-
-    body.insertBefore(backgroundImage, header);
     
 };
 
